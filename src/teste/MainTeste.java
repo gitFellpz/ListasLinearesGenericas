8b9e088fed8d52ef3aab4ac9ae9cfdc64c3248bf
gitFellpz <felipe.adrilima@gmail.com>
2023-05-02 09:42:59 -0300

    Pacote e class para teste concluido

---
+++ src/teste/MainTeste.java
@@ -0,0 +1,26 @@
+package teste;
+
+import java.util.Scanner;
+
+import listas.ListaIntCrescente;
+
+public class MainTeste {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		ListaIntCrescente lista = new ListaIntCrescente();
+		System.out.println("Informe dado positivo para inserir ou negativo para encerrar");
+		
+		int valor = input.nextInt();
+		while (valor >= 0) {
+			lista.insere(valor);
+			lista.exibir();
+			System.out.println("Informe dado positivo para inserir ou negativo para encerrar ");
+			valor = input.nextInt();
+		}
+
+		input.close();
+	}
+
+}
