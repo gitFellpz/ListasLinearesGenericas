510f140338a5a97f69e40d0ae4894487ef3f5812
gitFellpz <felipe.adrilima@gmail.com>
2023-05-16 09:10:34 -0300

    criação de class 'ListaAlunos'

---
+++ src/listas/ListaAlunos.java
@@ -0,0 +1,76 @@
+package listas;
+
+public class ListaAlunos {
+	private class NO{
+		int dado;
+		NO prox;
+	}
+	
+	private NO lista = null;
+	//Lista vazia (sem opreação init)
+	
+	public void insere(int elem) {
+		NO novo = new NO();
+		novo.dado = elem;
+		
+		if(lista==null) {
+			novo.prox = null;
+			lista = novo;
+		}
+		else {
+			if(novo.dado < lista.dado) {
+				//o novo nó vai se tornar o primeiro da lista
+				novo.prox = lista;
+				lista = novo;
+			}
+			else {
+				//o novo nó vai no 'meio' da lista (posição != da primeira)
+				NO aux = lista;
+				boolean achou = false;
+				
+				while(aux.prox != null && !achou) {
+					if(aux.prox.dado < novo.dado)
+						aux = aux.prox;
+					else
+						achou = true;
+				}
+				
+				novo.prox = aux.prox;
+				aux.prox = novo;
+			}
+		}
+		
+	}
+	
+	public void exibir() {
+		NO aux = lista;
+		int i = 1;
+		
+		while(aux != null) {
+			System.out.print("\n" + i + " NO: " + aux.dado);
+			i++;
+			aux = aux.prox;
+		}
+	}
+	
+	public void remover(int valor) {
+		//Ver se é o primeiro da fila
+		if(lista.dado == valor) {
+			lista = lista.prox;
+		}
+		else {
+			NO aux = lista;
+			boolean achou = false;
+			while(aux.prox != null && !achou) {
+				if(aux.prox.dado != valor) 
+					aux = aux.prox;
+				else {
+					achou = true;
+					aux.prox = aux.prox.prox;
+				}
+			}
+		}
+	}
+	
+	public void apresentaMaiores(int limite) {
+}
