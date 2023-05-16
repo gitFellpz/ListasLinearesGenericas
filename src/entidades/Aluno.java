4e3417659fb289b415f91a34ba157d3dc2e7283b
gitFellpz <felipe.adrilima@gmail.com>
2023-05-16 09:14:03 -0300

    Criação de construtor e getter and setters de Alunos

--- src/entidades/Aluno.java
+++ src/entidades/Aluno.java
@@ -1,5 +1,28 @@
 package entidades;
 
 public class Aluno {
-
+	private int rm;
+	private double media;
+	
+	public Aluno(int rm, double media) {
+		super();
+		this.rm = rm;
+		this.media = media;
+	}
+	
+	//Getter e setters
+	public int getRm() {
+		return rm;
+	}
+	public void setRm(int rm) {
+		this.rm = rm;
+	}
+	public double getMedia() {
+		return media;
+	}
+	public void setMedia(double media) {
+		this.media = media;
+	}
+	
+	
 }
